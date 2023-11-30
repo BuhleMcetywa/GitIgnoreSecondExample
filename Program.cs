2038421c2126be4d50055a39dca97c046db9d836
BuhleMcetywa <buhlemcetywa07@gmail.com>
2023-11-30 13:47:27 +0200

    Added Loop to Hello World

--- Program.cs
+++ Program.cs
@@ -3,8 +3,11 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {    for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello, World!");
+                Console.WriteLine("Goodbye World");
+            }
         }
     }
 }
